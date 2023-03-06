5c096877d03195f210d87668e096e99710f7f902
ZHONGZHIHONG <2398329945@qq.com>
2023-03-06 16:10:34 +0800

    插入用户

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -6,6 +6,8 @@ import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,11 +25,22 @@ public class UserService {
         HashMap<String, Object> map = new HashMap<>();
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerPhone());
 
         // 判断用户信息是否存在
+        if (passengerUsers.size() == 0) {
+            // 如果不存在，插入用户信息
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setState((byte) 0);
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUser.setProfilePhoto(null);
 
-        // 如果不存在，插入用户信息
+            passengerUserMapper.insert(passengerUser);
+        }
 
         return ResponseResult.success();
     }
