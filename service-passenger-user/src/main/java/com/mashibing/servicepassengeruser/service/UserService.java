69c65639318c8a9677448f31200291547329b570
ZHONGZHIHONG <2398329945@qq.com>
2023-03-06 15:57:08 +0800

    根据手机号查询用户信息

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -1,15 +1,29 @@
 package com.mashibing.servicepassengeruser.service;
 
+import com.mashibing.internalcommon.dto.PassengerUser;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    PassengerUserMapper passengerUserMapper;
+
     public ResponseResult LoginOrRegister(String passengerPhone) {
 
-        System.out.println("UserService:"+passengerPhone);
+        System.out.println("UserService:" + passengerPhone);
 
         // 根据手机号查询用户信息
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("passenger_phone", passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerPhone());
 
         // 判断用户信息是否存在
 
