78d637aee6f0b755a5cafec69a44f1898dac3c79
ZHONGZHIHONG <2398329945@qq.com>
2023-03-09 16:39:05 +0800

    插入司机信息代码编写完成

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
@@ -6,14 +6,29 @@ import com.mashibing.servicedriveruser.mapper.DriverUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 public class DriverUserService {
 
     @Autowired
     DriverUserMapper driverUserMapper;
 
+    //测试代码
     public ResponseResult getUser() {
         DriverUser driverUser = driverUserMapper.selectById(1);
         return ResponseResult.success(driverUser);
+    }
+
+    //添加司机信息
+    public ResponseResult addUser(DriverUser driverUser) {
+
+        LocalDateTime now = LocalDateTime.now();
+        driverUser.setGmtCreate(now);
+        driverUser.setGmtModified(now);
+
+        driverUserMapper.insert(driverUser);
+        return ResponseResult.success("");
     }
 }
