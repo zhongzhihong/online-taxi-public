47c69bfde7540996c40c549d5936cd53f416cbd6
ZHONGZHIHONG <2398329945@qq.com>
2023-03-09 22:33:14 +0800

    添加车辆接口代码编写&测试

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
@@ -1,20 +1,34 @@
-package com.mashibing.serviceDriverUser.controller;
+package com.mashibing.servicedriveruser.controller;
 
 
+import com.mashibing.internalcommon.dto.Car;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicedriveruser.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 钟志宏
  * @since 2023-03-09
  */
-@Controller
-@RequestMapping("/car")
+@RestController
 public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @PostMapping("/car")
+    public ResponseResult addUser(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
 
 }
