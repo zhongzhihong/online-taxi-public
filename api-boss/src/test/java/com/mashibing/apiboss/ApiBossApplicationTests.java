b4a84a1eb4b27b251bd2cc3bf8f24d9fac89f465
ZHONGZHIHONG <2398329945@qq.com>
2023-03-09 17:57:51 +0800

    api-boss服务创建并注册到nacos中

---
+++ api-boss/src/test/java/com/mashibing/apiboss/ApiBossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.apiboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
