457b20701e7f5c28844848c0ac07848d144c2ec5
ZHONGZHIHONG <2398329945@qq.com>
2023-03-09 16:44:19 +0800

    service-driver-user注册到nacos

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,8 +3,10 @@ package com.mashibing.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.mashibing.servicedriveruser.mapper")
 public class ServiceDriverUserApplication {
 
--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
@@ -6,7 +6,6 @@ import com.mashibing.servicedriveruser.mapper.DriverUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
