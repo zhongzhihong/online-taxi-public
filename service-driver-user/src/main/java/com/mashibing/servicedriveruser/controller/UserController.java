01a6446c3ca9fa1686b301ee1fd469fa51c108d3
ZHONGZHIHONG <2398329945@qq.com>
2023-03-10 17:23:08 +0800

    service-driver-user根据手机号查询司机是否存在代码实现

--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/UserController.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/UserController.java
@@ -2,12 +2,10 @@ package com.mashibing.servicedriveruser.controller;
 
 import com.mashibing.internalcommon.dto.DriverUser;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.response.DriverUserExistsResponse;
 import com.mashibing.servicedriveruser.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -23,6 +21,25 @@ public class UserController {
     @PutMapping("/user")
     public ResponseResult updateUser(@RequestBody DriverUser driverUser) {
         return driverUserService.updateUser(driverUser);
+    }
+
+    @GetMapping("/check-driver/{driverPhone}")
+    public ResponseResult getDriverUserByPhone(@PathVariable String driverPhone) {
+
+        ResponseResult<DriverUser> driverUserByPhone = driverUserService.getDriverUserByPhone(driverPhone);
+        DriverUser DriverUserDB = driverUserByPhone.getData();
+        DriverUserExistsResponse response = new DriverUserExistsResponse();
+        int IfExists = 1;
+        if (DriverUserDB == null) {
+            IfExists = 0;
+            response.setDriverPhone(driverPhone);
+            response.setIfExists(IfExists);
+        } else {
+            response.setDriverPhone(DriverUserDB.getDriverPhone());
+            response.setIfExists(IfExists);
+        }
+
+        return ResponseResult.success(response);
     }
 
 }
--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
@@ -1,5 +1,7 @@
 package com.mashibing.servicedriveruser.service;
 
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
+import com.mashibing.internalcommon.constant.DriverCarConstants;
 import com.mashibing.internalcommon.dto.DriverUser;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.servicedriveruser.mapper.DriverUserMapper;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class DriverUserService {
@@ -39,5 +43,19 @@ public class DriverUserService {
 
         driverUserMapper.updateById(driverUser);
         return ResponseResult.success("");
+    }
+
+    //根据手机号查询司机是否存在
+    public ResponseResult<DriverUser> getDriverUserByPhone(String driverPhone) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("driver_phone", driverPhone);
+        map.put("state", DriverCarConstants.DRIVER_STATE_VALID);
+
+        List<DriverUser> driverUsers = driverUserMapper.selectByMap(map);
+        if (driverUsers.isEmpty()) {
+            return ResponseResult.fail(CommonStatusEnum.DRIVER_NOT_EXIST.getCode(), CommonStatusEnum.DRIVER_NOT_EXIST.getValue());
+        }
+        DriverUser driverUser = driverUsers.get(0);
+        return ResponseResult.success(driverUser);
     }
 }
