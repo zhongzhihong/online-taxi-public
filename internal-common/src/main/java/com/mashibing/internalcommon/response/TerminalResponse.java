19f815ea0e4c7a8c2946d290291c47edf14a3aee
ZHONGZHIHONG <2398329945@qq.com>
2023-03-12 11:03:35 +0800

    车辆终端管理创建终端代码实现

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/response/TerminalResponse.java
@@ -0,0 +1,10 @@
+package com.mashibing.internalcommon.response;
+
+import lombok.Data;
+
+@Data
+public class TerminalResponse {
+
+    private String tid;
+
+}
---
+++ service-map/src/main/java/com/mashibing/servicemap/feign/TerminalClient.java
@@ -0,0 +1,44 @@
+package com.mashibing.servicemap.feign;
+
+import com.mashibing.internalcommon.constant.AMapConfigConstants;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.response.TerminalResponse;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TerminalClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${URL.key}")
+    private String URLKey;
+
+    @Value("${URL.sid}")
+    private String sid;
+
+    public ResponseResult add(String name) {
+        // 拼装请求的url
+        StringBuilder url = new StringBuilder();
+        url.append(AMapConfigConstants.TERMINAL_ADD);
+        url.append("?key=" + URLKey);
+        url.append("&sid=" + sid);
+        url.append("&name=" + name);
+        System.out.println("创建终端的URL为：" + url);
+
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(url.toString(), null, String.class);
+        Object body = stringResponseEntity.getBody();
+        JSONObject result = JSONObject.fromObject(body);
+        JSONObject data = result.getJSONObject("data");
+        String tid = data.getString("tid");
+        TerminalResponse terminalResponse = new TerminalResponse();
+        terminalResponse.setTid(tid);
+        return ResponseResult.success(terminalResponse);
+    }
+
+}
