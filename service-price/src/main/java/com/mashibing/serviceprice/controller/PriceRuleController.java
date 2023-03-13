2981803f7fb742125c35537e071d7e7a675fecf3
ZHONGZHIHONG <2398329945@qq.com>
2023-03-13 21:23:21 +0800

    service-price服务修改计价规则代码编写

--- service-price/src/main/java/com/mashibing/serviceprice/controller/PriceRuleController.java
+++ service-price/src/main/java/com/mashibing/serviceprice/controller/PriceRuleController.java
@@ -31,4 +31,9 @@ public class PriceRuleController {
         return priceRuleService.add(priceRule);
     }
 
+    @PostMapping("/edit")
+    public ResponseResult edit(@RequestBody PriceRule priceRule) {
+        return priceRuleService.edit(priceRule);
+    }
+
 }
--- service-price/src/main/java/com/mashibing/serviceprice/service/PriceRuleService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/PriceRuleService.java
@@ -1,6 +1,7 @@
 package com.mashibing.serviceprice.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.PriceRule;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.serviceprice.mapper.PriceRuleMapper;
@@ -21,7 +22,7 @@ public class PriceRuleService {
         //拼接并设置FareType
         String cityCode = priceRule.getCityCode();
         String vehicleType = priceRule.getVehicleType();
-        String fareType = cityCode + vehicleType;
+        String fareType = cityCode + "$" + vehicleType;
         priceRule.setFareType(fareType);
 
         //添加版本号
@@ -33,7 +34,46 @@ public class PriceRuleService {
         List<PriceRule> priceRules = priceRuleMapper.selectList(queryWrapper);
         Integer fareVersion = 0;
         if (priceRules.size() > 0) {
-            fareVersion = priceRules.get(0).getFareVersion();
+            return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_EXISTS.getCode(), CommonStatusEnum.PRICE_RULE_EXISTS.getValue());
+        }
+        priceRule.setFareVersion(++fareVersion);
+
+        priceRuleMapper.insert(priceRule);
+        return ResponseResult.success();
+    }
+
+    public ResponseResult edit(PriceRule priceRule) {
+
+        //拼接并设置FareType
+        String cityCode = priceRule.getCityCode();
+        String vehicleType = priceRule.getVehicleType();
+        String fareType = cityCode + "$" + vehicleType;
+        priceRule.setFareType(fareType);
+
+        //添加版本号
+        QueryWrapper<PriceRule> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("city_code", cityCode);
+        queryWrapper.eq("vehicle_type", vehicleType);
+        queryWrapper.orderByDesc("fare_version");
+
+        List<PriceRule> priceRules = priceRuleMapper.selectList(queryWrapper);
+        Integer fareVersion = 0;
+        if (priceRules.size() > 0) {
+            PriceRule latestPriceRule = priceRules.get(0);
+            Double startFare = latestPriceRule.getStartFare();
+            Integer startMile = latestPriceRule.getStartMile();
+            Double unitPricePerMile = latestPriceRule.getUnitPricePerMile();
+            Double unitPricePerMinute = latestPriceRule.getUnitPricePerMinute();
+
+            if (startFare.doubleValue() == priceRule.getStartFare().doubleValue()
+                    && startMile.intValue() == priceRule.getStartMile().intValue()
+                    && unitPricePerMile.doubleValue() == priceRule.getUnitPricePerMile().doubleValue()
+                    && unitPricePerMinute.doubleValue() == priceRule.getUnitPricePerMinute().doubleValue()
+            ) {
+                return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_NOT_EDIT.getCode(), CommonStatusEnum.PRICE_RULE_NOT_EDIT.getValue());
+            }
+
+            fareVersion = latestPriceRule.getFareVersion();
         }
         priceRule.setFareVersion(++fareVersion);
 
