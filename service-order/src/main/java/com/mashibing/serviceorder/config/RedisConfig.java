1f8785f8be8fd582f31ceb5641a8815d8a09fa49
ZHONGZHIHONG <2398329945@qq.com>
2023-03-16 10:13:52 +0800

    redis代码优化

--- service-order/src/main/java/com/mashibing/serviceorder/config/RedisConfig.java
+++ service-order/src/main/java/com/mashibing/serviceorder/config/RedisConfig.java
@@ -3,16 +3,26 @@ package com.mashibing.serviceorder.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
 
+    private String prefix = "redis://";
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("192.168.75.157:6379").setDatabase(0);
+        // address = redis://192.168.75.157:6379
+        config.useSingleServer().setAddress(prefix + redisHost + ":" + redisPort).setDatabase(0);
 
         return Redisson.create(config);
     }
