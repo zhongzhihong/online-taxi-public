f82c6d2916e44bd756e20fe7a26346e379291fbc
ZHONGZHIHONG <2398329945@qq.com>
2023-03-17 11:20:54 +0800

    service-pay服务添加到到nacos注册中心中

--- service-pay/src/main/java/com/mashibing/servicepay/ServicePayApplication.java
+++ service-pay/src/main/java/com/mashibing/servicepay/ServicePayApplication.java
@@ -2,11 +2,13 @@ package com.mashibing.servicepay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ServicePayApplication {
 
     public static void main(String[] args) {
