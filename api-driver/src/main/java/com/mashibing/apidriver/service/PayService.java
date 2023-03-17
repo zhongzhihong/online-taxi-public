a998e405c36fcd9719bb9c57be91b1df4bab9110
ZHONGZHIHONG <2398329945@qq.com>
2023-03-17 09:41:24 +0800

    api-driver服务司机发起收款骨架代码编写

---
+++ api-driver/src/main/java/com/mashibing/apidriver/service/PayService.java
@@ -0,0 +1,13 @@
+package com.mashibing.apidriver.service;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayService {
+
+    public ResponseResult pushPayInfo(String orderId, String price, Long passengerId) {
+        return null;
+    }
+
+}
